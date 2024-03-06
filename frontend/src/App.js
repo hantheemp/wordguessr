fa3fe7fc69c0be9543fc4b54586f24ce22f72ba5
hantheemp <murat.temel@bahcesehir.edu.tr>
2024-03-07 02:02:58 +0300

    frontend init fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Pages/Home/Home';
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
+import Footer from './Components/Footer/Footer';
+import { useState } from "react";
+import { IntlProvider } from "react-intl";
+import translations from "./Translations";
+import ForgotPass from './Pages/ForgotPass/ForgotPass';
+import ResetPass from './Pages/ResetPass/ResetPass';
 
 function App() {
+
+  const [locale, setLocale] = useState('EN');
+
+  const [menu, setMenu] = useState("home");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>App</h1>
   );
 }
 
---
+++ frontend/src/Translations/index.js
@@ -0,0 +1,9 @@
+import tr from './tr.json'
+import en from './en.json'
+
+const translations = {
+    EN: en,
+    TR: tr
+}
+
+export default translations
