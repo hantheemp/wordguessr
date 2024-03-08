a95aa2153c2061cf65092e0f602648e00458c91b
hantheemp <murat.temel@bahcesehir.edu.tr>
2024-03-08 15:42:21 +0300

    signup init

--- frontend/src/Pages/SignUp/SignUp.jsx
+++ frontend/src/Pages/SignUp/SignUp.jsx
@@ -1,13 +1,102 @@
-import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
-import signup from "./signup.js";
+import {
+  Box,
+  Button,
+  ChakraProvider,
+  Checkbox,
+  Container,
+  Divider,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  HStack,
+  Input,
+  Link,
+  Stack,
+  Text,
+  Spacer,
+} from "@chakra-ui/react";
+import Logo from "../../Components/Assets/Logo.png";
+import OAuthButtonGroup from "./OAuthButtonGroup.jsx";
+import PasswordField from "./PasswordField.jsx";
+import signin from "./signup.js";
 
-const SignUp = () => {
-  return (
-    <ChakraProvider theme={signup}>
-      <h1>SignUp</h1>
-    </ChakraProvider>
-  );
-};
+const SignUp = () => (
+  <ChakraProvider theme={signin}>
+    <Container
+      maxW="lg"
+      py={{
+        base: "12",
+        md: "24",
+      }}
+      px={{
+        base: "0",
+        sm: "8",
+      }}
+    >
+      <Stack spacing="8">
+        <Stack spacing="6">
+          <Flex className="flex-logo">
+            <img src={Logo} alt="Logo" />
+          </Flex>
+          <Stack
+            spacing={{
+              base: "2",
+              md: "3",
+            }}
+            textAlign="center"
+          ></Stack>
+        </Stack>
+        <Box
+          py={{
+            base: "0",
+            sm: "8",
+          }}
+          px={{
+            base: "4",
+            sm: "10",
+          }}
+          bg={{
+            base: "transparent",
+            sm: "bg.surface",
+          }}
+          boxShadow={{
+            base: "none",
+            sm: "md",
+          }}
+          borderRadius={{
+            base: "none",
+            sm: "xl",
+          }}
+        >
+          
+
+          <Stack spacing="6">
+            <Stack spacing="5">
+              <FormControl>
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <Input id="email" type="email" />
+              </FormControl>
+              <PasswordField />
+              <PasswordField />
+            </Stack>
+            <HStack justify="space-between"></HStack>
+            <Stack spacing="6">
+              <Button>Sign up</Button>
+              <HStack>
+                <Divider />
+                <Text textStyle="sm" whiteSpace="nowrap" color="fg.muted">
+                  or continue with
+                </Text>
+                <Divider />
+              </HStack>
+              <OAuthButtonGroup />
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Container>
+  </ChakraProvider>
+);
 
 export default SignUp;
--- frontend/src/Pages/SignUp/signup.js
+++ frontend/src/Pages/SignUp/signup.js
@@ -1,5 +1,21 @@
 import { extendTheme } from "@chakra-ui/react";
 
-const signup = extendTheme({});
+const signup = extendTheme({
+  styles: {
+    global: {
+      ".flex-logo": {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "1rem",
+      },
+      ".flex-logo img": {
+        width: "30%",
+        height: "30%",
+        borderRadius: "50%",
+      },
+    },
+  },
+});
 
 export default signup;
